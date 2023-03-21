2edd5c2b9cfc73834ef1cf01c9cdaa594f17ca3b
sajuni11 <sajuni11@dotincorp.com>
2023-03-21 10:13:02 +0900

    컨트롤러 GET 메소드 쿼리파라미터로 받아오게 수정

--- src/main/java/com/blog/search/controller/SearchController.java
+++ src/main/java/com/blog/search/controller/SearchController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +21,7 @@ public class SearchController{
     private final SearchService searchService;
 
     @GetMapping("/search")
-    public ResponseEntity<?> getSearchKeyword(@RequestBody @Valid KeywordSearchReqDTO reqParam) {
+    public ResponseEntity<?> getSearchKeyword(@Valid KeywordSearchReqDTO reqParam) {
         return new ResponseEntity<>(searchService.getKeywordSearch(reqParam), HttpStatus.OK);
     }
 
